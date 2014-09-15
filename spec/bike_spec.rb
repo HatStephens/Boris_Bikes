89b197e3e7ac83fe0a81862ca01f952db9e39a36
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-15 17:18:00 +0100

    First Commit - 2 spec tests

---
+++ spec/bike_spec.rb
@@ -0,0 +1,16 @@
+require 'bike'
+
+describe Bike do
+
+	it 'should not be broken after we create it' do
+		the_bike = Bike.new
+		expect(the_bike).not_to be_broken
+	end
+
+	it 'can be broken' do
+		the_bike = Bike.new
+		the_bike.break!
+		expect(the_bike).to be_broken
+	end
+
+end
