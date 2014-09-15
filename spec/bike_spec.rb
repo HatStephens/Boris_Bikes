9dfb761c6756dbb429cdf7b2a0e0995cf89eabcc
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-15 17:44:50 +0100

    Three rspec tests after refactoring.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,20 +1,22 @@
-require 'bike'
+require 'bike' #'./lib/bike'
 
 describe Bike do
 
+	let(:the_bike) { Bike.new }
+
 	it 'should not be broken after we create it' do
-		the_bike = Bike.new
+		#the_bike = Bike.new
 		expect(the_bike).not_to be_broken
 	end
 
 	it 'can be broken' do
-		the_bike = Bike.new
-		the_bike.break!
-		expect(the_bike).to be_broken
+		#the_bike = Bike.new
+		#the_bike.break!
+		expect(the_bike.break!).to be_broken
 	end
 
 	it "can be fixed" do
-		the_bike = Bike.new
+		#the_bike = Bike.new
 		#the_bike.break!
 		#the_bike.fix!
 		expect(the_bike.break!.fix!).not_to be_broken
