ad29f0eb796746f0fff3d4f0e1eb6f091e49fd94
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-15 17:31:43 +0100

    2nd Commit - third rspec test.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -13,4 +13,11 @@ describe Bike do
 		expect(the_bike).to be_broken
 	end
 
+	it "can be fixed" do
+		the_bike = Bike.new
+		#the_bike.break!
+		#the_bike.fix!
+		expect(the_bike.break!.fix!).not_to be_broken
+	end
+
 end
